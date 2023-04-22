7217fb060055b409c9139a8126b246ccedde096b
Anonymous User <71043332+darunkle@users.noreply.github.com>
2023-04-21 21:10:11 -0400

    added interview questions and logic

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
 setup(name='docassemble.WageTheftApp',
       version='0.0.1',
       description=('A docassemble extension.'),
-      long_description='# docassemble.WageTheftApp\n\nA docassemble extension.\n\n## Author\n\ndrunkle@suffolk.edu\n\n',
+      long_description='# docassemble.WageTheftApp\r\n\r\nA docassemble extension.\r\n\r\n## Author\r\n\r\ndrunkle@suffolk.edu\r\n\r\n',
       long_description_content_type='text/markdown',
       author='',
       author_email='drunkle@suffolk.edu',
